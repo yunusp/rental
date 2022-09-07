b43650240ad497405b9a377653b80b6dce1bf1fd
yunusp <yunuspoonawala52@gmail.com>
2022-09-07 22:59:23 +0530

    todo list-esque persists in cookies

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::collections::HashMap;
 
 use rocket::{
+    form::Form,
     get,
     http::{Cookie, CookieJar},
-    launch, routes,
+    launch, post,
+    response::Redirect,
+    routes, uri, FromForm,
 };
 use rocket_dyn_templates::{context, Template};
 
@@ -41,12 +44,50 @@ fn template(jar: &CookieJar) -> Template {
     Template::render("index", context! {names: cs})
 }
 
+#[get("/temp/ui")]
+fn show_ui(jar: &CookieJar) -> Template {
+    let mut cs = HashMap::new();
+    jar.iter().for_each(|c| {
+        cs.insert(c.name(), c.value());
+    });
+    Template::render("ui", context! {cs: cs})
+}
+
+#[derive(FromForm)]
+struct FormData {
+    name: String,
+    value: String,
+}
+
+#[post("/temp/ui", data = "<data>")]
+fn add_cookie(jar: &CookieJar, data: Form<FormData>) -> Redirect {
+    jar.add(Cookie::new(data.name.to_owned(), data.value.to_owned()));
+    Redirect::to(uri!("/temp/ui"))
+}
+
+#[get("/temp/ui/clear")]
+fn reset_cookies(jar: &CookieJar) -> Redirect {
+    for cookie in jar.iter() {
+        jar.remove(Cookie::named(cookie.name()).into_owned());
+    }
+    Redirect::to(uri!("/temp/ui"))
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount(
             "/",
-            routes![index, greet, set_cookie, get_cookies, template],
+            routes![
+                index,
+                greet,
+                set_cookie,
+                get_cookies,
+                template,
+                show_ui,
+                add_cookie,
+                reset_cookies
+            ],
         )
         .attach(Template::fairing())
 }
