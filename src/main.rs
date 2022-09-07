1d25c7db464cf23f3a3bfd1c88fb1d3465c8c2a7
yunusp <yunuspoonawala52@gmail.com>
2022-09-07 20:13:35 +0530

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{fs::relative, fs::FileServer, get, launch, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world!"
+}
+
+#[get("/hello/<name>")]
+fn greet(name: &str) -> String {
+    format!("Hello, {name}")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, greet])
+        .mount("/", FileServer::from(relative!("static")))
 }
