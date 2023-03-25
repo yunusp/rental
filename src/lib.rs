788c5e57b2d60498ec332d6f4bba1412e8bfdbef
yunusp <yunuspoonawala52@gmail.com>
2023-03-25 18:51:28 +0530

    extract cors stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
 use sha2::{self, Sha256, Digest};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response, options};
 
 pub fn sha256sum(s: &str) -> String {
     let mut hasher = Sha256::new();
     hasher.update(s.as_bytes());
     format!("{:X}", hasher.finalize()) //:X = hexadecimal uppercase
+}
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+/// Catches all OPTION requests in order to get the CORS related Fairing triggered.
+#[options("/<_..>")]
+pub fn all_options() {
+    /* Intentionally left empty */
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod models;
 mod repo;
 use std::{collections::HashMap, sync::Mutex};
 
-use rocket::{launch, routes, options};
+use rental::{all_options, CORS};
+use rocket::{launch, routes};
 use rocket_dyn_templates::Template;
 
 use crate::controllers::{
@@ -13,39 +14,6 @@ use crate::controllers::{
     signup::{g_sign_up, p_sign_up},
 };
 use crate::repo::user_repo::UserRepo;
-
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::{Request, Response};
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, GET, PATCH, OPTIONS",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
-
-/// Catches all OPTION requests in order to get the CORS related Fairing triggered.
-#[options("/<_..>")]
-fn all_options() {
-    /* Intentionally left empty */
-}
-
 
 #[launch]
 async fn rocket() -> rocket::Rocket<rocket::Build> {
@@ -56,7 +24,14 @@ async fn rocket() -> rocket::Rocket<rocket::Build> {
         .manage(ctx)
         .mount(
             "/",
-            routes![g_index, g_sign_in, p_sign_in, g_sign_up, p_sign_up, all_options],
+            routes![
+                g_index,
+                g_sign_in,
+                p_sign_in,
+                g_sign_up,
+                p_sign_up,
+                all_options
+            ],
         )
         .attach(Template::fairing())
         .attach(CORS)
