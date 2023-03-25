e5a90755a648cdc7b3cc9f3d2ca4367a41eab896
yunusp <yunuspoonawala52@gmail.com>
2023-03-25 21:06:31 +0530

    signup "works"
    So much databse changes needed

--- src/controllers/signup.rs
+++ src/controllers/signup.rs
@@ -14,38 +14,46 @@ pub async fn g_sign_up(ctx: &State<Mutex<HashMap<String, String>>>) -> Template 
     ctx.lock().unwrap().remove("uname_unavail");
     Template::render("signup", lock)
 }
-#[derive(FromForm)]
+#[derive(Debug, FromForm)]
 pub struct SignUpForm {
     uname: String,
+    email: String,
+    phone_number: String,
+    adhaar_number: String,
     pass: String,
     pass1: String,
+    photo: String,
+    birthday: String,
 }
-#[post("/signup", data = "<data>")] 
+
+#[post("/signup", data = "<data>")]
 pub async fn p_sign_up(
     ctx: &State<Mutex<HashMap<String, String>>>,
     db: &State<UserRepo>,
     data: Form<SignUpForm>,
-) -> Redirect {
-    if data.pass != data.pass1 {
-        let mut ctx = ctx.lock().unwrap();
-        ctx.insert("no_pass_match".to_string(), "a".to_string());
-        return Redirect::to("/signup");
-    }
-    let new_user = User {
-        id: None,
-        uname: data.uname.to_owned(),
-        pass: sha256sum(&data.pass),
-    };
-    match db.add_user(new_user).await {
-        Some(resp) => {
-            resp.unwrap();
-        }
-        None => {
-            ctx.lock()
-                .unwrap()
-                .insert("uname_unavail".to_string(), "true".to_string());
-            return Redirect::to(uri!("/signup"));
-        }
-    }
-    Redirect::to(uri!("/"))
+) -> String {
+    println!("{data:?}");
+    "Hello".to_string()
+    // if data.pass != data.pass1 {
+    //     let mut ctx = ctx.lock().unwrap();
+    //     ctx.insert("no_pass_match".to_string(), "a".to_string());
+    //     return Redirect::to("/signup");
+    // }
+    // let new_user = User {
+    //     id: None,
+    //     uname: data.uname.to_owned(),
+    //     pass: sha256sum(&data.pass),
+    // };
+    // match db.add_user(new_user).await {
+    //     Some(resp) => {
+    //         resp.unwrap();
+    //     }
+    //     None => {
+    //         ctx.lock()
+    //             .unwrap()
+    //             .insert("uname_unavail".to_string(), "true".to_string());
+    //         return Redirect::to(uri!("/signup"));
+    //     }
+    // }
+    // Redirect::to(uri!("/"))
 }
